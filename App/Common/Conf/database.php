a2a6781255c0ca76c94442256d342ff7db342e66
xiaohai <ishaiua@163.com>
2017-06-30 22:31:52 +0800

    update readme

--- App/Common/Conf/database.php
+++ App/Common/Conf/database.php
@@ -4,23 +4,23 @@ return array(
 //*************************************数据库设置*************************************
     'DB_TYPE'               =>  'mysql',                 // 数据库类型
     'DB_HOST'               =>  '127.0.0.1',     // 服务器地址
-    'DB_NAME'               =>  'admin',     // 数据库名
+    'DB_NAME'               =>  'haiua',     // 数据库名
     'DB_USER'               =>  'root',     // 用户名
     'DB_PWD'                =>  'root',      // 密码
     'DB_PORT'               =>  '3306',     // 端口
-    'DB_PREFIX'             =>  'hai_',   // 数据库表前缀
+    'DB_PREFIX'             =>  'hh_',   // 数据库表前缀
     'RBAC_DB_DSN'  =>   array(
         'DB_TYPE'       =>  'mysql',      
         'DB_HOST'       =>  '127.0.0.1',  
-        'DB_NAME'       =>  'admin',           
+        'DB_NAME'       =>  'haiua',           
         'DB_USER'       =>  'root',      
         'DB_PWD'        =>  'root',     
         'DB_PORT'       =>  '3306',       
-        'DB_PREFIX'     =>  'hai_',  
+        'DB_PREFIX'     =>  'hh_',  
     ),    //数据库连接DSN
     
-    'RBAC_ROLE_TABLE'=>'hai_role' ,     //角色表名称
-    'RBAC_USER_TABLE' =>'hai_role_user',     //用户表名称
-    'RBAC_ACCESS_TABLE'=>'hai_access' ,       //权限表名称
-    'RBAC_NODE_TABLE' => 'hai_node' ,   //节点表名称
+    'RBAC_ROLE_TABLE'=>'hh_role' ,     //角色表名称
+    'RBAC_USER_TABLE' =>'hh_role_user',     //用户表名称
+    'RBAC_ACCESS_TABLE'=>'hh_access' ,       //权限表名称
+    'RBAC_NODE_TABLE' => 'hh_node' ,   //节点表名称
 );
--- README.md
+++ README.md
@@ -2,3 +2,33 @@
 
 基于ThinkPHP3.2的多模块后台管理系统
 
+使用
+
+1.git clone https://github.com/ishaiua/ThinkPHP-admin.git
+
+2.在该项目目录运行： php -S localhost:8000
+
+3.在浏览器中输入：localhost:8000   
+4.then You can see:
+
+
+
+![step1](http://ohgi3imno.bkt.clouddn.com/admin/stet1.png)
+![step2](http://ohgi3imno.bkt.clouddn.com/admin/stet2.png)
+![step3](http://ohgi3imno.bkt.clouddn.com/admin/stet3.png)
+![step4](http://ohgi3imno.bkt.clouddn.com/admin/stet4.png)
+![login](http://ohgi3imno.bkt.clouddn.com/admin/login.png)
+![node](http://ohgi3imno.bkt.clouddn.com/admin/node.png)
+![fish](http://ohgi3imno.bkt.clouddn.com/admin/fishrecord.png)
+
+
+- 其他的自己发现
+
+主要功能
+
+基于RBAC的权限控制
+
+分页的美化
+
+
+
